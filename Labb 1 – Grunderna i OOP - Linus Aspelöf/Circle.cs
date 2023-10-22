49f7361bc62a0f1358beab8f5ceb799d61c69780
Linus Aspelöf <Tederollz@hotmail.com>
2023-10-22 16:44:19 +0200

    Add project files.

---
+++ Labb 1 – Grunderna i OOP - Linus Aspelöf/Circle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb_1___Grunderna_i_OOP___Linus_Aspelöf
+{
+    internal class Circle
+    {
+        public double Radius;
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public double GetArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+}
---
+++ Labb 1 – Grunderna i OOP - Linus Aspelöf/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Labb_1___Grunderna_i_OOP___Linus_Aspelöf
+// Linus Aspelöf SUT23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle1 = new Circle(5);
+            Circle circle2 = new Circle(6);
+
+            double area1 = circle1.GetArea();
+            double area2 = circle2.GetArea();
+
+            Console.WriteLine($"\n\tArea av cirkel 1 med radie 5: {area1}" +
+                $"\n\tArea av cirkel 2 med radie 6: {area2}");
+            Console.ReadKey();
+        }
+    }
+}
